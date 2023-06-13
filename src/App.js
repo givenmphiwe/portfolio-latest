5b85a8b049abfaf4a34446a505efb9dbb4fa707f
givenmphiwe <giggslive52@gmail.com>
2023-06-13 13:52:28 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import landingImg from "./assets/landing.png";
-import ssg from "./assets/ssg icon.png"
-import page from "./assets/Page icon.png"
-import biztweak from "./assets/biztweak logo.png"
-import bets from "./assets/136bets icon.png"
+import ssg from "./assets/ssg icon.png";
+import page from "./assets/Page icon.png";
+import biztweak from "./assets/biztweak logo.png";
+import bets from "./assets/136bets icon.png";
+import bets2 from "./assets/136bets.jpg"
 
 function App() {
   return (
@@ -19,138 +20,203 @@ function App() {
       <div id="About" className="container-1">
         <h3>About</h3>
         <p>
-          As a web & android developer, I enjoy bridging the gab between design and engineering
-           -- combining my technical knowledge with my keen eye for design to create a beautiful product.My goal
-           is to always build applications that are scalable and efficient under the hood while providing engaging,
-           pixel-perfect user experiences.
-           <br/>
-           
-           <b>When am not in front of a computer,</b> I'm probably reading books, walking or playing video games.
-           <br/>
-           <br/>
-           <a href="#"><b>View my Resume --&gt;</b></a>
-
+          As a web & android developer, I enjoy bridging the gab between design
+          and engineering -- combining my technical knowledge with my keen eye
+          for design to create a beautiful product.My goal is to always build
+          applications that are scalable and efficient under the hood while
+          providing engaging, pixel-perfect user experiences.
+          <br />
+          <b>When am not in front of a computer,</b> I'm probably reading books,
+          walking or playing video games.
+          <br />
+          <br />
+          <a href="#">
+            <b>View my Resume --&gt;</b>
+          </a>
         </p>
-    
       </div>
 
-      <div id="section-1"  className="container-consult">
+      <div id="section-1" className="container-consult">
         <button>Website Projects</button>
       </div>
 
       <div className="container-2">
         <h3></h3>
-        <div onClick={()=> window.open("https://www.biztweak.org.za/", "_blank")} className="card">
+        <div
+          onClick={() => window.open("https://www.biztweak.org.za/", "_blank")}
+          className="card"
+        >
           <div className="imgbox">
             <img src={biztweak} alt="Png.png" />
           </div>
           <div className="content">
             <p>
               <h3>Biztweak</h3>
-              The purpose of bookkeeping is to maintain a systematic record of
-              financial activities and transactions chronologically. The purpose
-              of accounting is to report the financial strength and obtain the
-              results of the operating activity of a business
+              Web app to make entrepreneurship easier. Being an entrepreneur is
+              an interesting journey for sure, especially when you don’t know
+              what you don’t know about your business or business idea. Not
+              knowing the right information at the right time has caused failure
+              in many businesses.
+            </p>
+            <br />
+            <p>
+              <b>Click to view</b>
             </p>
           </div>
         </div>
 
-        <div onClick={()=> window.open("https://pagefinancialservices-8ba30.web.app/", "_blank")} className="card">
+        <div
+          onClick={() =>
+            window.open(
+              "https://pagefinancialservices-8ba30.web.app/",
+              "_blank"
+            )
+          }
+          className="card"
+        >
           <div className="imgbox">
             <img className="pageimg" src={page} alt="Png.png" />
           </div>
           <div className="content">
             <p>
               <h3>Page Financial Services</h3>
-              In South Africa, there is no set payroll cycle. The payroll cycle
-              is typically stipulated in the employment contract, which is to be
-              agreed upon by the employee and employer. The payroll cycle can
-              run monthly, weekly, or bi-weekly.
+              Website to assist business with solving challenges that migh arise
+              when starting or when running your businnes, Page Provides
+              services bookkeeping annual financial statements to web
+              development and many more
+            </p>
+            <br />
+            <p>
+              <b>Click to view</b>
             </p>
           </div>
         </div>
 
-        <div onClick={()=> window.open("https://stylesservicesgr.com/", "_blank")} className="card">
+        <div
+          onClick={() => window.open("https://stylesservicesgr.com/", "_blank")}
+          className="card"
+        >
           <div className="imgbox">
             <img src={ssg} alt="Png.png" />
           </div>
           <div className="content">
             <p>
               <h3>Styles Services Group</h3>
-              A more comprehensive list of tasks to
-              which Web development commonly refers, may include Web
-              engineering, Web design, Web content development, client liaison,
-              client-side/server-side scripting, Web server and network security
-              configuration, and e-commerce development.
+              supplier of full service solutions to the Hospitality industry,
+              train,place quality of staff. They are committed to providing the
+              best services to stand out in their industry with our service.
+            </p>
+            <br />
+            <p>
+              <b>Click to view</b>
             </p>
           </div>
         </div>
 
-        
-        <div onClick={()=> window.open("https://onethreesix-21ba8.web.app/#", "_blank")} className="card">
+        <div
+          onClick={() =>
+            window.open("https://onethreesix-21ba8.web.app/#", "_blank")
+          }
+          className="card"
+        >
           <div className="imgbox">
             <img className="pageimg" src={bets} alt="Png.png" />
           </div>
           <div className="content">
             <p>
               <h3>136Bets</h3>
-              Refer to the annual presentation of the entity’s financial
-              performance.They comprise a Balance Sheet, Statement of Profit and
-              Loss, Statement of changes in equity, Cash flow statement, and
-              Notes to the financial statements.
+              Is a mock of a betting app just some simple design and expressing
+              love of software development in the app
+            </p>
+            <br />
+            <p>
+              <b>Click to view</b>
             </p>
           </div>
         </div>
       </div>
 
-      <div id="section-1"  className="container-consult">
+      <div id="section-1" className="container-consult">
         <button>Android App Projects</button>
       </div>
 
       <div className="container-2">
         <h3></h3>
-        <div onClick={()=> window.open("https://play.google.com/store/apps/details?id=octavia.dictionary.siswati&hl=en&gl=US", "_blank")}  className="card">
+        <div
+          onClick={() =>
+            window.open(
+              "https://play.google.com/store/apps/details?id=octavia.dictionary.siswati&hl=en&gl=US",
+              "_blank"
+            )
+          }
+          className="card"
+        >
           <div className="imgbox">
             <img src="{bookkeeping}" alt="Png.png" />
           </div>
           <div className="content">
             <p>
               <h3>Siswati Dictionary</h3>
-              The purpose of bookkeeping is to maintain a systematic record of
-              financial activities and transactions chronologically. The purpose
-              of accounting is to report the financial strength and obtain the
-              results of the operating activity of a business
+              Siswati dictionary has words to assist learn siswati in an easy
+              and exciting manner.Siswati dictionary it's designed to help you
+              improve your learning of siswati as it also translate siswati to
+              english.
+            </p>
+            <br />
+            <p>
+              <b>Click to view</b>
             </p>
           </div>
         </div>
 
-        <div onClick={()=> window.open("https://play.google.com/store/apps/details?id=given.teleios&hl=en&gl=US", "_blank")}  className="card">
+        <div
+          onClick={() =>
+            window.open(
+              "https://play.google.com/store/apps/details?id=given.teleios&hl=en&gl=US",
+              "_blank"
+            )
+          }
+          className="card"
+        >
           <div className="imgbox">
             <img src="{payroll}" alt="Png.png" />
           </div>
           <div className="content">
             <p>
               <h3>Teleios</h3>
-              In South Africa, there is no set payroll cycle. The payroll cycle
-              is typically stipulated in the employment contract, which is to be
-              agreed upon by the employee and employer. The payroll cycle can
-              run monthly, weekly, or bi-weekly.
+              offer several 24-hour services to maintain trouble-free fire and
+              security systems that include: fire and intrusion monitoring,
+              remote administration and troubleshooting and repair
+            </p>
+            <br />
+            <p>
+              <b>Click to view</b>
             </p>
           </div>
         </div>
 
-        <div onClick={()=> window.open("https://play.google.com/store/apps/details?id=com.AmaCrop.Abhii&hl=en&gl=US", "_blank")} className="card">
+        <div
+          onClick={() =>
+            window.open(
+              "https://play.google.com/store/apps/details?id=com.AmaCrop.Abhii&hl=en&gl=US",
+              "_blank"
+            )
+          }
+          className="card"
+        >
           <div className="imgbox">
             <img src="developer} " alt="Png.png" />
           </div>
           <div className="content">
             <p>
-              <h3>Abhii</h3>
-              A more comprehensive list of tasks to
-              which Web development commonly refers, may include Web
-              engineering, Web design, Web content development, client liaison,
-              client-side/server-side scripting, Web server and network security
-              configuration, and e-commerce development.
+              <h3>Abhii</h3> a strategy board game that two to three people can
+              play. The players are in the race to get home, where they movement
+              is determined by the single dice.This game also support offline
+              mode
+            </p>
+            <br />
+            <p>
+              <b>Click to view</b>
             </p>
           </div>
         </div>
@@ -170,23 +236,33 @@ function App() {
             </p>
           </div>
         </div>
-        <div onClick={()=> window.open("https://onethreesix-21ba8.web.app/Home/Home.html", "_blank")} className="card">
+        <div
+          onClick={() =>
+            window.open(
+              "https://onethreesix-21ba8.web.app/Home/Home.html",
+              "_blank"
+            )
+          }
+          className="card"
+        >
           <div className="imgbox">
             <img className="pageimg" src={bets} alt="Png.png" />
           </div>
           <div className="content">
             <p>
               <h3>136Bets</h3>
-              Refer to the annual presentation of the entity’s financial
-              performance.They comprise a Balance Sheet, Statement of Profit and
-              Loss, Statement of changes in equity, Cash flow statement, and
-              Notes to the financial statements.
+              Is a mock of a betting app just some simple design and expressing
+              love of software development in the app
+            </p>
+            <br />
+            <p>
+              <b>Click to view</b>
             </p>
           </div>
         </div>
       </div>
 
-      <Footer  />
+      <Footer />
     </>
   );
 }
